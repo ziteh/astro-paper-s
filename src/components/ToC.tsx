21b9958ce277c502ceba9c3d4f03975f12195ded
ziteh <honmonoh@gmail.com>
2024-09-21 11:12:56 +0800

    feat: update ToC

--- src/components/ToC.tsx
+++ src/components/ToC.tsx
@@ -1,22 +1,34 @@
-import type { TocItem } from "@utils/toc";
+import type { TocTree } from "@utils/toc";
 
 interface Props {
-  toc: TocItem[];
+  toc: TocTree[];
+}
+
+function TreeNode({ item }: { item: TocTree }) {
+  return (
+    <div className={`toc-lv-${item.level}`}>
+      <li key={item.id} style={{ marginLeft: `${(item.level - 1) * 1.25}rem` }}>
+        <a href={`#${item.id}`}>{item.text}</a>
+        {item.children.length > 0 && <SubTree toc={item.children} />}
+      </li>
+    </div>
+  );
+}
+
+function SubTree({ toc }: Props) {
+  return (
+    <ul>
+      {toc.map(item => (
+        <TreeNode item={item} />
+      ))}
+    </ul>
+  );
 }
 
 export default function ToC({ toc }: Props) {
   return (
     <div className="toc">
-      <ul>
-        {toc.map(item => (
-          <li
-            key={item.id}
-            style={{ marginLeft: `${(item.level - 1) * 1.25}rem` }}
-          >
-            <a href={`#${item.id}`}>{item.text}</a>
-          </li>
-        ))}
-      </ul>
+      <SubTree toc={toc} />
     </div>
   );
 }
--- src/utils/toc.ts
+++ src/utils/toc.ts
@@ -1,23 +1,52 @@
 import * as cheerio from "cheerio";
 
-export interface TocItem {
+export interface TocTree {
   id: string;
   text: string;
   level: number;
+  children: TocTree[];
 }
 
-export function getTocFromHtml(content: string) {
-  const toc: TocItem[] = [];
+export function getTocFromHtml(content: string): TocTree[] {
+  const toc: TocTree[] = [];
   const $ = cheerio.load(content);
 
   $("h1, h2, h3").each((_, element) => {
     const level = parseInt(element.tagName[1], 10);
     const text = $(element).text();
     const id = $(element).attr("id") || getAnchorText(text);
-    toc.push({ id, text, level });
+
+    const newItem: TocTree = { id, text, level, children: [] };
+
+    // If there are existing items, find the appropriate parent
+    if (toc.length > 0) {
+      let currentLevel = level;
+      let parent = toc;
+
+      while (currentLevel > 1) {
+        const lastItem = parent[parent.length - 1];
+        if (lastItem.level < currentLevel) {
+          lastItem.children.push(newItem);
+          return; // Exit after adding to the correct parent
+        }
+        parent = lastItem.children;
+        currentLevel--;
+      }
+
+      // If we reach here, it means the new item is at the same level as the top level
+      toc.push(newItem);
+    } else {
+      toc.push(newItem);
+    }
   });
 
   return toc;
+}
+
+export interface TocItem {
+  id: string;
+  text: string;
+  level: number;
 }
 
 export function getTocFromMarkdown(content: string) {
