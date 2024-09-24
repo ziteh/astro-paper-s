584b86d19e633655479fa6e54b5bbfa727600932
ziteh <honmonoh@gmail.com>
2024-09-24 18:48:02 +0800

    feat: improve flex on mobile

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -24,11 +24,13 @@ export default function ListItem({ href, frontmatter }: Props) {
     <li className="my-1">
       <a
         href={href}
-        className="group flex flex-row space-x-4 rounded-md p-2 hover:bg-skin-card/30"
+        className="flex flex-col space-x-4 rounded-md p-2 hover:bg-skin-card/30 sm:flex-row"
       >
-        <Datetime date={updated ?? date} />
+        <span className="mr-4 text-skin-accent/80">
+          <Datetime date={updated ?? date} />
+        </span>
         <span>{title}</span>
-        <span className="hidden opacity-50 md:block">{subtitle}</span>
+        <span className="opacity-50">{subtitle}</span>
       </a>
     </li>
   );
