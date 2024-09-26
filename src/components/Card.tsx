004a1892adcf11d321a0ae527460166f57aa5e3b
ziteh <honmonoh@gmail.com>
2024-09-26 20:33:02 +0800

    feat: update tags margin

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -84,9 +84,9 @@ export default function Card({
             >
               <path d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8 8a2 2 0 0 0 2.828 0l7.172-7.172a2 2 0 0 0 0-2.828l-8-8zM7 9a2 2 0 1 1 .001-4.001A2 2 0 0 1 7 9z"></path>
             </svg>
-            <div className="flex flex-wrap space-x-2">
-              {tags.map(tag => (
-                <p className="text-sm" key={tag}>
+            <div className="flex flex-wrap">
+              {tags.map((tag: string) => (
+                <p className="mr-2 text-sm" key={tag}>
                   #{tag}
                 </p>
               ))}
