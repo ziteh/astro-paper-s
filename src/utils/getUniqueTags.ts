c2b7134bf5c9fcfc45a4198f85841a1d1b707b01
ziteh <honmonoh@gmail.com>
2025-05-17 00:25:56 +0800

    feat: add tag count to unique tags and update Tag component rendering

--- src/utils/getUniqueTags.ts
+++ src/utils/getUniqueTags.ts
@@ -1,22 +1,39 @@
-import type { CollectionEntry } from "astro:content";
 import { slugifyStr } from "./slugify";
+import type { CollectionEntry } from "astro:content";
 import postFilter from "./postFilter";
 
 interface Tag {
   tag: string;
   tagName: string;
+  count: number;
 }
 
 const getUniqueTags = (posts: CollectionEntry<"blog">[]) => {
-  const tags: Tag[] = posts
+  const tagCountMap = new Map<string, Tag>();
+
+  posts
     .filter(postFilter)
     .flatMap(post => post.data.tags)
-    .map(tag => ({ tag: slugifyStr(tag), tagName: tag }))
-    .filter(
-      (value, index, self) =>
-        self.findIndex(tag => tag.tag === value.tag) === index
-    )
-    .sort((tagA, tagB) => tagA.tag.localeCompare(tagB.tag));
+    .forEach(tag => {
+      const slugTag = slugifyStr(tag);
+      if (tagCountMap.has(slugTag)) {
+        // Tag existing
+        const existingTag = tagCountMap.get(slugTag)!;
+        existingTag.count += 1;
+      } else {
+        // New Tag
+        tagCountMap.set(slugTag, {
+          tag: slugTag,
+          tagName: tag,
+          count: 1,
+        });
+      }
+    });
+
+  const tags = Array.from(tagCountMap.values()).sort((tagA, tagB) =>
+    tagA.tag.localeCompare(tagB.tag)
+  );
+
   return tags;
 };
 
