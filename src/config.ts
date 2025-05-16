48fc84dd3961379876471dfd7b73b23360ecfcc4
ziteh <honmonoh@gmail.com>
2025-05-16 21:09:20 +0800

    feat: disable 'back button' by default

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const SITE = {
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
-  showBackButton: true, // show back button in post detail
+  showBackButton: false, // show back button in post detail
   editPost: {
     enabled: false,
     text: "Suggest Changes",
