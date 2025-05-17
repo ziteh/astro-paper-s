cdf7c7f5fd375f375a124bb319bd703e57a55fda
ziteh <honmonoh@gmail.com>
2025-05-17 09:44:07 +0800

    feat: use Expressive Code instead of Shiki for code syntax highlighting

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,20 @@
+import { SITE } from "./src/config";
 import { defineConfig } from "astro/config";
+import fs from "node:fs";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
-import { SITE } from "./src/config";
+import expressiveCode, { ExpressiveCodeTheme } from "astro-expressive-code";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+
+const jsoncString = fs.readFileSync(
+  new URL(`./theme/mod-min-light.jsonc`, import.meta.url),
+  "utf-8"
+);
+const modMinLight = ExpressiveCodeTheme.fromJSONString(jsoncString);
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +23,38 @@ export default defineConfig({
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
+    expressiveCode({
+      plugins: [pluginLineNumbers()],
+      themes: ["one-dark-pro", modMinLight],
+      themeCssSelector: theme => {
+        if (theme.name === "one-dark-pro") {
+          return "[data-theme='dark']";
+        }
+        return "[data-theme='light']";
+      },
+      defaultProps: {
+        wrap: false,
+        showLineNumbers: false,
+        overridesByLang: {
+          "bash,cmd,powershell,ps,sh,shell,zsh": { frame: false },
+        },
+      },
+      styleOverrides: {
+        codeFontFamily: "'Roboto Mono', 'Noto Sans TC', monospace",
+        uiFontFamily:
+          "'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+        borderWidth: "0",
+        textMarkers: {
+          backgroundOpacity: "33%",
+          inlineMarkerBorderWidth: "0.1px",
+        },
+        frames: {
+          editorTabBarBackground: "transparent",
+          frameBoxShadowCssValue: "transparent",
+          tooltipSuccessBackground: "#6b7280",
+        },
+      },
+    }),
   ],
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
@@ -21,11 +62,12 @@ export default defineConfig({
       rehypeSlug,
       [rehypeAutolinkHeadings, { behavior: "append" }],
     ],
-    shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
-      themes: { light: "min-light", dark: "night-owl" },
-      wrap: true,
-    },
+    // Use ExpressiveCode instead of shiki
+    // shikiConfig: {
+    //   // For more themes, visit https://shiki.style/themes
+    //   themes: { light: "min-light", dark: "night-owl" },
+    //   wrap: true,
+    // },
   },
   vite: {
     plugins: [tailwindcss()],
