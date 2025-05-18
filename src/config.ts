8dccc3a4149c9855b1fd6ac8c88d821694016ce9
ziteh <honmonoh@gmail.com>
2025-05-18 09:12:58 +0800

    feat: update site configuration and social links

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain, e.g. https://ziteh.github.io/
-  author: "Sat Naing", // 君の名は ~
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://astro-paper-s.ziteh.dev", // replace this with your deployed domain, e.g. https://ziteh.github.io/
+  author: "ZiTe", // 君の名は ~
+  profile: "https://github.com/ziteh",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme. A fork of AstroPaper theme",
+  title: "AstroPaper-S",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -23,5 +23,5 @@ export const SITE = {
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
   langOg: "en_US", // Open Graph locale tag, format 'language_TERRITORY' https://ogp.me/#optional
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/Taipei", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
